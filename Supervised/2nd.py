d67bc94e88a7c66f2c51008ae1cce68ac8b2cf40
Nivrutti Chavan <chavannivrutti130@gmail.com>
2026-07-23 17:44:05 +0530

    predicting student will pass or fail based on study of hours using LOGISTIC_REGRESSION

---
+++ Supervised/2nd.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import LogisticRegression
+X=[[1],[2],[3],[4],[5]]
+y=[0,0,1,1,1]
+
+model=LogisticRegression()
+model.fit(X,y)
+
+Hours=float(input("Enter study of Hours="))
+
+Result=model.predict([[Hours]])[0]
+if Result==1:
+    print(f"from your study of {Hours} Hours You may be likely to PASS")
+else:
+    print(f"from your study of {Hours} Hours You may be likely to FAIL")
